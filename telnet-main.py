3c9266ed93e2f2da7e73e236a80b2c11855baec6
Satoru Nakamura <jijitarou1888@yahoo.co.jp>
2017-01-12 15:46:57 +0900

    telmod.py

--- telnet-main.py
+++ telnet-main.py
@@ -1,1 +1,18 @@
-print ("telnet main for network use...")
+import telmod
+
+r1 = {"address":"192.168.44.10",
+    "r_type":"cisco",
+    "login_id":"satoru",
+    "login_pass":"satoru",
+    "second_pass":"satoru"}
+
+telcl = telmod.RemoteAccess(**r1)
+
+res = telcl.init_telnet()
+print(res)
+
+res = telcl.send_com("sh ip int brief")
+print(res)
+
+res = telcl.disconnect()
+print(res)
